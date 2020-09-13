ba53566c69cd1a5154b1a5afd1fb87e593209feb
DESKTOP-F5QML8E\Alex <golan4ik@gmail.com>
2020-09-13 22:36:16 +0300

    test

--- frontend/src/utils/networking.js
+++ frontend/src/utils/networking.js
@@ -112,6 +112,7 @@ export const getSongLyrics = (songPath, songId) => {
 };
 
 export const addToFavorites = (songId) => {
+  console.log(songId);
   return axios
     .post(ADD_TO_FAVORITES_URL, {
       songId,
